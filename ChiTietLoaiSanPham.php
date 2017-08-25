b39cc9420aacc987b86ff6778bbe3675640be7e4
Nhatluong <luongquocnhat@gmail.com>
2017-08-25 08:51:08 +0700

    ok

---
+++ ChiTietLoaiSanPham.php
@@ -0,0 +1,243 @@
+<html lang="en">
+    <title></title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link type="text/css" rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="Css/Styles.css">
+        <script type="text/javascript" src="Jquery/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="Bootstrap/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+        <div class="container banner">
+            <div class="row col-md-12">
+                <div class="col-md-3 col-xs-12 col-sm-6">
+                    <img src="img/logo.png">
+                </div>
+                <div class="col-md-6 col-xs-12 hidden-xs hidden-md hidden-sm ">
+                    <div class="form-inline" >
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="tim kiem">
+                            <button type="submit" class="btn btn-warning">Tim Kiem</button>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p>Hotline: <strong class="hotline">1900 451 452</strong></p>
+                    <p>Email: support@mobile.com</p>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-inverse ">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home"></span> </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Trang chủ</a></li>
+                        <li><a href="#">Giới thiệu</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Danh mục sản phẩm <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Apple</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Samsung</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Oppo</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">HTC</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Asus</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Bphone</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Sony</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-left hidden-lg " role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Tìm kiếm">
+                        </div>
+                        <button type="submit" class="btn btn-default">Tìm kiếm</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon glyphicon-shopping-cart"> </span>Gió hàng </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"> </span><span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Đăng nhập</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Đăng ký</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <div class="container-fluid" bh>
+            <div class="breadcrumb"><a href="#">Trang chủ</a>/<a href="#">Laptop</a> </div>
+        </div>
+        <div class="container content1">
+            <div class="row col-md-12">
+                <div class="col-md-3 content1-left">
+                    <div class="text-center danhmuc"><STRONG>DANH MỤC SẢN PHẨM</STRONG></div>
+                    <div class="list-group">
+                        <li class="list-group-item"><a href="#">Apple</a> </li>
+                        <li class="list-group-item"><a href="#">Samsung</a> </li>
+                        <li class="list-group-item"><a href="#">Oppo</a> </li>
+                        <li class="list-group-item"><a href="#">HTC</a> </li>
+                        <li class="list-group-item"><a href="#">Asus</a> </li>
+                        <li class="list-group-item"><a href="#">Bphone</a> </li>
+                        <li class="list-group-item"><a href="#">Sony</a> </li>
+
+                    </div>
+                </div>
+                <div class="col-md-8 col-md-offset-1 content1-right">
+                    <div class="col-md-12 text-center theloai"><strong>Apple</strong></div>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/SONY%20VAIO%20SVF1.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/SONY%20VAIO%20SVF1.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/SONY%20VAIO%20SVF1.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/SONY%20VAIO%20SVF1.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/Macbook%20Air%202015.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                                <img data-src="holder.js/300x200" alt=".." src="img/laptop/lenovo%20yoga500.png">
+                                <div class="caption">
+                                    <h4>Sony Vaio SVF1</h4>
+                                    <p> 19.990.000₫</p>
+                                    <p><a href="#" class="btn btn-warning" role="button">Mua hàng</a>
+                                        <a href="#" class="btn btn-warning" role="button">Xem hàng</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid content2">
+            <div class="row contenttop">
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-send">&nbsp;</span>MIỄN PHÍ VẬN CHUYỂN</strong></p>
+                    Miễn phí vận chuyển các tỉnh nội thành Hà Nội & TP. HCM
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><span class="glyphicon glyphicon-send">&nbsp;</span><strong>ĐỔI TRẢ TRONG VÒNG 24H</strong></p>
+                    <P>Đổi trả sản phẩm nhanh chóng trong vòng 24h đồng hồ</P>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-send">&nbsp;</span>ĐỔI TRẢ TRONG VÒNG 24H</strong></p>
+                    <P>Đổi trả sản phẩm nhanh chóng trong vòng 24h đồng hồ</P>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-send">&nbsp;</span>ĐỔI TRẢ TRONG VÒNG 24H</strong></p>
+                    <P>Đổi trả sản phẩm nhanh chóng trong vòng 24h đồng hồ</P>
+                </div>
+            </div>
+            <div class="row contentup">
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-info-sign">&nbsp;</span> THÔNG TIN CÔNG TY</strong></p>
+                    <ul>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-info-sign">&nbsp;</span>THÔNG TIN CÔNG TY</strong></p>
+                    <ul>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-info-sign">&nbsp;</span>THÔNG TIN CÔNG TY</strong></p>
+                    <ul>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <p><strong><span class="glyphicon glyphicon-info-sign">&nbsp;</span>THÔNG TIN CÔNG TY</strong></p>
+                    <ul>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-tags">&nbsp;</span> Thông tin công ty</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid footer">
+            <div class="col-md-12 text-center">
+                <p >© Bản quyền thuộc về TT TEAM - Cung cấp bởi BIZWEB</p>
+            </div>
+        </div>
+
+    </body>
+</html>
